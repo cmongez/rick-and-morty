ded6b1bb4518b7df407d9be72d9fec7f1864091f
Cesar <camongez28@gmail.com>
2022-06-20 14:17:26 -0400

    home fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href=""><router-link to="/home">Home</router-link></a>
+          <a class="nav-link" href=""><router-link to="/">Home</router-link></a>
         </li>
  
         
